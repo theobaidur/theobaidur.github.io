65d41eaa92b85934959e5e8ba7e2b451a1595168
Obaidur Rahman <rahman@valencesystems.com>
2016-05-03 15:36:24 +0600

    project 56 update

---
+++ project_56/js/app.js
@@ -0,0 +1,286 @@
+if (window.location.protocol == 'file:') {
+    alert('To test this demo properly please use a local server such as XAMPP or WAMP. See README.md for more details.');
+}
+(function($, window, document, undefined) {
+    //config 
+    var config = {
+        "sizes": [
+                {"small": {"width": 800, "height": 600}},
+                {"medium": {"width": 1024, "height": 768}},
+                {"large": {"width": 1280, "height": 1024}}
+            ]
+    }
+
+    var QueryString = function () {
+      // This function is anonymous, is executed immediately and 
+      // the return value is assigned to QueryString!
+      var query_string = {};
+      var query = window.location.search.substring(1);
+      var vars = query.split("&");
+      for (var i=0;i<vars.length;i++) {
+        var pair = vars[i].split("=");
+            // If first entry with this name
+        if (typeof query_string[pair[0]] === "undefined") {
+          query_string[pair[0]] = decodeURIComponent(pair[1]);
+            // If second entry with this name
+        } else if (typeof query_string[pair[0]] === "string") {
+          var arr = [ query_string[pair[0]],decodeURIComponent(pair[1]) ];
+          query_string[pair[0]] = arr;
+            // If third or later entry with this name
+        } else {
+          query_string[pair[0]].push(decodeURIComponent(pair[1]));
+        }
+      } 
+        return query_string;
+    }();
+
+
+    // feature detection for drag&drop upload
+
+    var isAdvancedUpload = function() {
+        var div = document.createElement('div');
+        return (('draggable' in div) || ('ondragstart' in div && 'ondrop' in div)) && 'FormData' in window && 'FileReader' in window;
+    }();
+
+
+    // applying the effect for every form
+
+    $('.box').each(function() {
+        var $form = $(this),
+            $input = $form.find('input[type="file"]'),
+            $label = $form.find('label'),
+            $errorMsg = $form.find('.box__error span'),
+            $restart = $form.find('.box__restart'),
+            droppedFiles = false,
+            processFiles = function(files) {
+                var imageIndex = -1;
+                for (var i = 0, f; (f = files[i]) && imageIndex < 0; i++) {
+                  // Only process image files.
+			      if (!f.type.match('image.*')) {
+			        continue;
+			      }
+
+                  imageIndex = i;
+			      var reader = new FileReader();
+
+			      // Closure to capture the file information.
+			      reader.onload = (function(theFile) {
+			        return function(e) {
+			          theFile["imageData"] = e.target.result;
+
+                      if(QueryString.w !== undefined || QueryString.h !== undefined){
+                        config["sizes"] = [{
+                            "custom":{
+                                width: QueryString.w || QueryString.h,
+                                height: QueryString.h || QueryString.w
+                            }
+                        }]
+                      }
+
+                      $(".crop-tool-area").empty();
+                      $(".button_row").show();
+
+                      for(i=0; config["sizes"][i];i++){
+                        resizeableImage(theFile, config["sizes"][i]);  
+                      }
+                      
+			        };
+			      })(f);
+
+			      // Read in the image file as a data URL.
+			      reader.readAsDataURL(f);
+			    }
+                $label.text(files[imageIndex].name);
+            };
+
+        // letting the server side to know we are going to make an Ajax request
+        $form.append('<input type="hidden" name="ajax" value="1" />');
+
+        // automatically submit the form on file select
+        $input.on('change', function(e) {
+            processFiles(e.target.files);
+
+
+            //	$form.trigger( 'submit' );
+
+
+        });
+
+
+        // drag&drop files if the feature is available
+        if (isAdvancedUpload) {
+            $form
+                .addClass('has-advanced-upload') // letting the CSS part to know drag&drop is supported by the browser
+                .on('drag dragstart dragend dragover dragenter dragleave drop', function(e) {
+                    // preventing the unwanted behaviours
+                    e.preventDefault();
+                    e.stopPropagation();
+                })
+                .on('dragover dragenter', function() //
+                    {
+                        $form.addClass('is-dragover');
+                    })
+                .on('dragleave dragend drop', function() {
+                    $form.removeClass('is-dragover');
+                })
+                .on('drop', function(e) {
+                    droppedFiles = e.originalEvent.dataTransfer.files; // the files that were dropped
+                    processFiles(droppedFiles);
+
+                });
+        }
+
+        // restart the form if has a state of error/success
+
+        $restart.on('click', function(e) {
+            e.preventDefault();
+            $form.removeClass('is-error is-success');
+            $input.trigger('click');
+        });
+
+        // Firefox focus bug fix for file input
+        $input
+            .on('focus', function() {
+                $input.addClass('has-focus');
+            })
+            .on('blur', function() {
+                $input.removeClass('has-focus');
+            });
+    });
+
+    var resizeableImage = function(image, frameInfo) {
+        // Some variable and settings
+        var container,
+        	overlay,
+            orig_src = new Image(),
+            image_target = $("<img>"),
+            event_state = {},
+            constrain = false,
+            min_width = 60, // Change as required
+            min_height = 60,
+            max_width = 800, // Change as required
+            max_height = 900,
+            resize_canvas = document.createElement('canvas');
+
+        init = function() {
+
+            var frameKey = Object.keys(frameInfo)[0];
+            // When resizing, we will always use this copy of the original as the base
+            orig_src.src = image.imageData;
+            image_target.attr("src",image.imageData);
+            //naming
+            var name = image.name.substr(0,image.name.lastIndexOf("."));
+            var ext = image.name.substr(image.name.lastIndexOf(".")+1);
+            var dim = frameInfo[frameKey]["width"]+"X"+frameInfo[frameKey]["height"];
+
+            image_target.data("name",name+"_"+dim+"."+ext);
+            
+            
+            var crop_tool = $("<div>").addClass("component").width(parseFloat(frameInfo[frameKey]["width"])+200)
+                                                            .height(parseFloat(frameInfo[frameKey]["height"])+200);
+            overlay = $("<div>").addClass("overlay")
+                                .width(parseFloat(frameInfo[frameKey]["width"]))
+                                .height(parseFloat(frameInfo[frameKey]["height"]))
+                                .css({left:100, top:100});
+                                
+            
+
+            var wrapper_draggable = $("<div>")
+                                    .addClass("draggable")
+                                    .addClass("resizable")
+                                    .width(orig_src.width)
+                                    .height(orig_src.height);
+
+
+            crop_tool.append(overlay);
+            crop_tool.append(wrapper_draggable);
+
+           wrapper_draggable.append(image_target); 
+
+
+            // Add events
+            wrapper_draggable.draggable({
+                        containment: "parent"
+                    }).resizable({
+                        handles: 'ne, se, sw, nw',
+                        aspectRatio: parseFloat(orig_src.width/orig_src.height)
+                    });
+            
+
+            var title = $("<h2>").text(frameKey+" ("+dim+")").addClass("frameTitle");
+            $(".crop-tool-area").append(title);
+            $(".crop-tool-area").append(crop_tool);
+
+
+        };
+
+        
+
+        crop = function() {
+            //Find the part of the image that is inside the crop box
+            var crop_canvas,
+                left = $('.overlay').offset().left - container.offset().left,
+                top = $('.overlay').offset().top - container.offset().top,
+                width = $('.overlay').width(),
+                height = $('.overlay').height();
+
+            crop_canvas = document.createElement('canvas');
+            crop_canvas.width = width;
+            crop_canvas.height = height;
+
+            console.log(crop_canvas.getContext('2d').drawImage(image_target, left, top, width, height, 0, 0, width, height));
+            window.open(crop_canvas.toDataURL("image/png"));
+        }
+
+        init();
+    };
+
+    $(".js-crop").click(function(e){
+        var zipContent = [];
+        $(".component").each(function(){
+            var imageOriginal = new Image();
+            var currentImage = $(this).find("img");
+            imageOriginal.src = currentImage.get(0).src;
+            var crop_canvas,
+                left = $(this).find('.overlay').offset().left - $(this).find(".draggable").offset().left,
+                top = $(this).find('.overlay').offset().top - $(this).find(".draggable").offset().top,
+                width = $(this).find('.overlay').width(),
+                height = $(this).find('.overlay').height();
+                
+            resize_canvas = document.createElement('canvas');
+            
+            resize_canvas.width = $(this).find(".resizable").width();
+            resize_canvas.height = $(this).find(".resizable").height();
+            resize_canvas.getContext('2d').drawImage(currentImage.get(0), 0, 0, $(this).find(".resizable").width(), $(this).find(".resizable").height());   
+            $(this).find("img").attr('src', resize_canvas.toDataURL("image/png"));  
+
+
+            crop_canvas = document.createElement('canvas');
+            crop_canvas.width = width;
+            crop_canvas.height = height;
+            //window.open($(this).find("img").get(0).src);
+            crop_canvas.getContext('2d').drawImage(currentImage.get(0), left, top, width, height, 0, 0, width, height);
+            var cropped_file_info = {
+                data : crop_canvas.toDataURL("image/png"),
+                name :  currentImage.data("name")
+            }
+            zipContent.push(cropped_file_info); 
+            currentImage.attr("src",imageOriginal.src);
+        });
+
+    var zip = new JSZip();
+
+
+    for($i=0; $i<zipContent.length; $i++){
+        zip.file(zipContent[$i].name, zipContent[$i].data);
+    }
+    zip.generateAsync({type:"blob"})
+        .then(function(content) {
+            saveAs(content, "cropped.zip");
+        });
+    });
+
+
+
+
+})(jQuery, window, document);
